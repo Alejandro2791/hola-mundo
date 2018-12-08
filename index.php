9286ba9917ded898266e7140028cab16f32f439b
Alejandro2791 <44538271+Alejandro2791@users.noreply.github.com>
2018-12-08 09:06:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-if($resultado > 0){
-echo "<table style='border-collapse: collapse;' align='center' border='1'>";
-echo "<tr id='parrafo'>";
-echo "<th>Nombre</th>";
-echo "<th>Año</th>";
-echo "</tr>";
-while ($columna = mysqli_fetch_array( $resultado ))
-{
-echo "<tr id='parrafo3'>";
-echo "<td>" . $columna['descripcion'] . "</td><td>" . $columna['anioEstreno'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-}else{
-echo "Error: ".$sql."<br>".mysqli_error($conexion);
-}
+
+echo "Hola Mundo!";
+
 ?>
