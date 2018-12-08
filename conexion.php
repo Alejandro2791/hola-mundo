930d2e9988242bca7246ebc8ffc81ca5ee18fd02
Alejandro2791 <44538271+Alejandro2791@users.noreply.github.com>
2018-12-08 13:27:19 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,7 @@
 <?php
 $db = pg_connect("host='ec2-54-163-230-178.compute-1.amazonaws.com' dbname=d73969j44gs1g8 port=5432 user=twqtfrzpcthcer password=166e51bc5a3acc6e007301c97255d40aa915918846424cd08fa0192fd7635d86") or die ("Error de Conexion".pg_last_error());
-//Variables
+
+//Variables de Insertar Datos
+$Nombre =$_POST['Nom'];
+$Fecha =$_POST['Fech']
 ?>
