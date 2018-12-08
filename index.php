b2a4ed411dd199dd24a70b480c99b92dcb17046c
Alejandro2791 <44538271+Alejandro2791@users.noreply.github.com>
2018-12-08 13:10:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+$db = pg_connect("host='ec2-54-163-230-178.compute-1.amazonaws.com' dbname=d73969j44gs1g8 port=5432 user=twqtfrzpcthcer password=166e51bc5a3acc6e007301c97255d40aa915918846424cd08fa0192fd7635d86") or die ("Error de Conexion".pg_last_error());
 echo "Hola Mundo!";
 
+//Variables
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Aplicacion - Hola Mundo</title>
+
+</head>
+<body>
+    <h3 id="subtitulo">Aplicacion</h3>
+    <hr color="blue" size=3 />
+    <h1 id="titulo">Agregar Empleado</h1>
+
+    <hr color="blue" size=3 />
+
+</body>
+</html>
