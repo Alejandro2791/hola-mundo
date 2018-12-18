bf62f6fa75d62913f76732e51d56cd68381a3e6e
Alejandro2791 <44538271+Alejandro2791@users.noreply.github.com>
2018-12-18 11:31:16 -0600

    Update Principal.php

--- Principal.php
+++ Principal.php
@@ -1,19 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Control de Empleados</title>
-    <link rel="stylesheet" type="text/css" href="StyleSheet.css">
-</head>
-<body id="body">
-    <h3 id="subtitulo">Control de Empleados</h3>
-    <hr color="blue" size=3>
-    <h1 id="titulo">Menu</h1>
 
-            <!–-   Creacion de Botones  -–>
-        <button id="boton" onclick="location.href='agregar.php'">Agregar Empleados</button>
-        &nbsp;&nbsp;
-        <button id="boton" onclick="location.href='Secundaria2.php'">Consultar Empleados</button>
-        &nbsp;&nbsp;
-</body>
-</html>
