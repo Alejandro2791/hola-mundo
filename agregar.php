28a68f2cd6477e8619485e63b1b2badab4724f61
ALEJANDRO <0alessx0@gmail.com>
2018-12-18 12:37:06 -0600

    Agregar Menu

--- agregar.php
+++ agregar.php
@@ -1,29 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Aplicacion - Hola Mundo</title>
-    <link rel="stylesheet" type="text/css" href="StyleSheet.css">
+<?php
+include("conexion.php");
 
-</head>
-<body id="body">
-    <h1 id="titulo">Aplicacion</h1>
-    <hr color="blue" size=3 />
-    <h3 id="titulo">Agregar Empleado</h3>
-    <hr color="blue" size=3 />
-    
-    <form method="post" action="conexion.php">
-        <p id="parrafo2">
-        <strong>Nombre:</strong> <input type="text" name="Nom" size="40">
-        &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;<strong>Fecha de Ingreso:</strong> <input type="text" name="Fech" size="10">
-    </p>
-
-    <p id="parrafo3">
-        <!–-   Creacion de Botones  -–>
-        <input id="boton" type="submit" value="Registrar" />
-
-    </p>
-    </form>
-
-</body>
-</html>
+//Variables de Insertar Datos
+$Nombre =$_POST['Nom'];
+$Fecha =$_POST['Fech'];
+  
+  $sql = "INSERT INTO Empleados (nombre, feching) VALUES ('$Nombre', '$Fecha')";
+  
+  if(pg_query($db, $sql)){
+echo "<script>
+alert('Registro nuevo');
+    window.location= 'agregar_empleados.php'
+</script>";;
+}else{
+echo "Error: ".$sql."<br>".pg_last_error($db);
+}
+?>
--- conexion.php
+++ conexion.php
@@ -6,18 +6,4 @@ $User = getenv('db_user');
 $Port = getenv('db_port');
 $db = pg_connect("host=$Host dbname=$Database port=$Port user=$User password=$Password") or die ("Error de Conexion".pg_last_error());
 
-//Variables de Insertar Datos
-$Nombre =$_POST['Nom'];
-$Fecha =$_POST['Fech'];
-  
-  $sql = "INSERT INTO Empleados (nombre, feching) VALUES ('$Nombre', '$Fecha')";
-  
-  if(pg_query($db, $sql)){
-echo "<script>
-alert('Registro nuevo');
-    window.location= 'index.php'
-</script>";;
-}else{
-echo "Error: ".$sql."<br>".pg_last_error($db);
-}
 ?>
